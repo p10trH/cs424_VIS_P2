51f240237c4aacfed75d536f0d6f45cbd3d94999
Peter <peterhanulajr@gmail.com>
2018-03-13 13:46:43 -0500

    Add files via upload

---
+++ app/server.R
@@ -0,0 +1,98 @@
+# Peter Hanula
+# Dimitar Kirilov
+# Tarush Vig
+
+# ---------------------------
+
+date <- rep('2017-01-01', each=24)
+times <- c('0030', '0100', '0200', '0300', '0400', '0500', '0623', '0700', '0800', '0900', '1034', '1100', '1233', '1300', '1445', '1500', '1600', '1700', '1812', '1900', '2011', '2111', '2200', '2356')
+data <- c(12, 55, 2, 37, 98, 33, 66, 41, 11, 12, 13, 6, 44, 45, 55, 89, 23, 24, 11, 2, 2, 3, 4, 5)
+
+#times <- lapply(times, as.character)
+
+hourFrame <- data.frame(date, times, data)
+
+hourFrame$fullDate <- paste(hourFrame$date, hourFrame$times, sep = " ")
+hourFrame$timesFormat <- as.POSIXct(hourFrame$fullDate, format = '%Y-%m-%d %H%M')
+
+chart  <- ggplot(hourFrame, aes(x = timesFormat, y = data)) + geom_line(na.rm = TRUE) + labs(x = NULL, y = NULL)
+chart2 <- ggplot(hourFrame, aes(x = timesFormat, y = data)) + geom_line(na.rm = TRUE) + labs(x = NULL, y = NULL) + scale_x_datetime(date_labels = "%H", date_breaks = "1 hour")
+chart3 <- ggplot(hourFrame, aes(x = timesFormat, y = data)) + geom_line(na.rm = TRUE) + labs(x = NULL, y = NULL) + scale_x_datetime(date_labels = "%H:%M", date_breaks = "3 hour")
+chart4 <- ggplot(hourFrame, aes(x = timesFormat, y = data)) + geom_line(na.rm = TRUE) + labs(x = NULL, y = NULL) + scale_x_datetime(date_labels = "%l %p", date_breaks = "3 hour")
+
+# ---------------------------
+
+statesData <-
+  read.csv(file = 'app/statesData.csv',
+           header = TRUE)
+
+states <- geojsonio::geojson_read("app/states.geojson", what = "sp")
+
+statesWData <- merge(states, statesData, by = "NAME")
+
+m <- leaflet(statesWData) %>%
+  setView(-96, 37.8, 4) %>%
+  addProviderTiles(providers$Stamen.TonerLite,
+                   options = providerTileOptions(noWrap = TRUE))
+
+bins <- c(0, 3, 6, 9, 12, 15, 18, 21, Inf)
+pal <- colorBin("YlOrRd", domain = statesWData$value, bins = bins)
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
+  statesWData$NAME, statesWData$value
+) %>% lapply(htmltools::HTML)
+
+m <- m %>% addPolygons(
+  fillColor = ~pal(statesWData$value),
+  weight = 2,
+  opacity = 1,
+  color = "black",
+  dashArray = "3",
+  fillOpacity = 0.7,
+  highlight = highlightOptions(
+    weight = 5,
+    color = "#666",
+    dashArray = "",
+    fillOpacity = 0.7,
+    bringToFront = TRUE),
+  label = labels,
+  labelOptions = labelOptions(
+    style = list("font-weight" = "normal", padding = "3px 8px"),
+    textsize = "15px",
+    direction = "auto")) %>% 
+  addLegend(pal = pal, values = ~value, opacity = 0.7, title = NULL,
+                                       position = "bottomright")
+
+# ---------------------------
+
+
+
+# Define server
+server <- function(input, output) {
+  
+  # output$hourlyArrDep <- renderPlot({
+  #   ggplot(data = hourlyOhareMelt, aes(x = Hour, 
+  #                                      y = value,
+  #                                      group = variable,
+  #                                      color = variable)) + 
+  #     labs(title = "O'Hare", x = "Time of Day (Hour)", y = "Num. of Flights", color = "Legend") +
+  #     geom_point() + 
+  #     geom_line(size = 1.5, alpha = 0.7)
+  # })
+  # 
+  # output$hourlyArrDep2 <- renderPlot({
+  #   ggplot(data = hourlyMidwayMelt, aes(x = Hour, 
+  #                                       y = value,
+  #                                       group = variable,
+  #                                       color = variable)) + 
+  #     labs(title = "Midway", x = "Time of Day (Hour)", y = "Num. of Flights", color = "Legend") +
+  #     geom_point() + 
+  #     geom_line(size = 1.5, alpha = 0.7)
+  # })
+  # 
+  # output$hourlyArrDepTable <- DT::renderDT(DT::datatable(hourlyOhare))
+  # 
+  # output$hourlyArrDep2Table <- DT::renderDT(DT::datatable(hourlyMidway))
+   
+}
---
+++ app/ui.R
@@ -0,0 +1,167 @@
+# Peter Hanula
+# Dimitar Kirilov
+# Tarush Vig
+
+library(shiny)
+library(shinyBS)
+library(shinythemes)
+library(shinydashboard)
+
+library(plyr)
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(reshape2)
+library(DT)
+library(leaflet)
+
+# ---------------------------
+
+# Define UI
+ui <- fluidPage(
+  
+  # Styling ---------------------------
+  
+  theme = shinytheme("darkly"),
+  tags$style(type='text/css', "body {background-color: #888888;}"),
+  
+  # tabsetPanel / tabBox / tabPanel
+  
+  tags$style(HTML("
+
+    .tabbable > .nav[id=mainNav] > li                     {font-size: 20px;}
+    .tabbable > .nav[id=mainNav] > li > a                 {color: #CFCFCF; border-radius: 4px; border: 0px;}
+    .tabbable > .nav[id=mainNav] > li[class=active] > a   {background-color: #484747; color: white; border: 0px;}
+    .tabbable > .nav[id=mainNav] > li > a:hover           {background-color: #484747;}
+
+    .tabbable > .nav[id=secondNav] > li > a               {color: #CFCFCF; border-radius: 4px; border: 4px solid #888888;}
+    .tabbable > .nav[id=secondNav] > li[class=active] > a {background-color: #888888; color: white; border: 4px solid #484747; }
+    .tabbable > .nav[id=secondNav] > li > a:hover         {background-color: #888888; border: 4px solid #484747;}
+
+    .tabbable > .nav > li > a                             {color: #CFCFCF; border-radius: 4px;}
+    .tabbable > .nav > li[class=active] > a               {background-color: #484747; color: white;}
+    .tabbable > .nav > li > a:hover                       {background-color: #484747;}
+                  
+    .nav-tabs-custom > .nav > li > a                      {color: #666666; border-radius: 4px; margin: 0px; padding: 10px;}
+    .nav-tabs-custom > .nav > li[class=active] > a        {background-color: #DDDDDD; color: #484747; border: 0px; margin: 1px;}
+    .nav-tabs-custom > .nav > li > a:hover                {background-color: #DDDDDD; border: 0px solid #DDDDDD; margin: 1px;}
+
+    .nav-tabs-custom > .nav > li                          {float: right;}
+    .pull-right                                           {float: left !important; font-size: 24px;}
+    .nav-tabs-custom > .nav                               {border-bottom: 0px;}")),
+  
+  tags$style(type='text/css', ".nav-tabs-custom {padding: 20px; margin-bottom: 20px; background-color: #AAAAAA; color: #484747; border-radius: 4px;}"),
+  tags$style(type='text/css', "#mainNav {background-color: #666666; border-radius: 4px; border-bottom: 1px solid #666666;}"), # margin: -85px 120px 0px 350px !important;
+  tags$style(type='text/css', "#secondNav {border-radius: 4px; border-bottom: 0px solid #188888; float: left; margin-top: -48px;}"),
+  
+  # Selectize input
+  
+  tags$style(type='text/css', ".selectize-input {background: #DDDDDD !important;} .selectize-dropdown {}"),
+  
+  # Modal windows
+  
+  tags$style(type='text/css', "#modal_Settings .modal-footer, #modal_About .modal-footer {display:none;}"),
+  
+  # Top title and buttons
+  
+  tags$style(type='text/css', "#action_Settings, #action_About {padding: 10px 10px 10px 10px; margin-top: 23px;}"),
+  tags$style(type='text/css', "h1 {font-size: 50px; font-weight: bold; color: #484747;}"),
+  
+  # App title & buttons  ---------------------------
+  
+  fluidRow(column(4, h1("Illinois Flights")), 
+           column(3, offset = 5, align = 'right',
+                  actionButton("action_Settings", label = "", icon = icon("cog", "fa-2x")),
+                  actionButton("action_About", label = "", icon = icon("info", "fa-2x")))),
+  br(),
+  
+  # Compare Airports ---------------------------
+  
+  tabsetPanel(id = "mainNav",
+              tabPanel("O'Hare vs Midway",
+                       br(),
+                       fluidRow(column(3, offset = 9, align = 'right',
+                                       checkboxInput("checkbox_scale", label = "Common Scale", value = TRUE))),
+                       
+  # Month ---------------------------
+                       
+                       tabsetPanel(id = "secondNav",
+                                   tabPanel("Month",
+                                            br(),
+                                            fluidRow(column(4,  selectInput("select_Month", label = NULL, 
+                                                                            choices = list("01 - January" = 1, "02 - February" = 2, "03 - March" = 3, "04 - April" = 4, "05 - May" = 5, "06 - June" = 6, "07 - July" = 7, "08 - August" = 8, "09 - September" = 9, "10 - October" = 10, "11 - November" = 11, "12 - December" = 12), 
+                                                                            selected = 1))),
+                                            fluidRow(
+                                              tabBox(
+                                                title = "Flights for Each Hour of the Day",
+                                                side = "left",
+                                                width = 12,
+                                                tabPanel("", icon = icon("table", "fa-2x"), br(),
+                                                         fluidRow(
+                                                           column(6,
+                                                                  #tags$style(type = "text/css", "#pieChart1 {min-height:40vh !important;}"),
+                                                                  DTOutput("hourlyArrDepTable")),
+                                                           column(6,
+                                                                  #tags$style(type = "text/css", "#pieChart2 {min-height:40vh !important;}"),
+                                                                  DTOutput("hourlyArrDep2Table"))
+                                                         )),
+                                                tabPanel("", icon = icon("line-chart", "fa-2x"), br(),
+                                                         fluidRow(
+                                                           column(6,
+                                                                  #tags$style(type = "text/css", "#pieChart1 {min-height:40vh !important;}"),
+                                                                  plotOutput("hourlyArrDep")),
+                                                           column(6,
+                                                                  #tags$style(type = "text/css", "#pieChart2 {min-height:40vh !important;}"),
+                                                                  plotOutput("hourlyArrDep2"))
+                                                         )))
+                                            )
+                                            
+                                            
+                                            
+                                            
+                                            ),
+  # Year ---------------------------
+  
+                                   tabPanel("Year", 
+                                            br(), h4("aaaaaaaa")))),
+              
+  # Single Airport ---------------------------
+              
+              tabPanel("Single Airport", h4("vvvvvvvvv"))),
+  
+  
+  
+  
+  
+  #  ---------------------------
+  
+  bsModal(id = "modal_Settings", "Settings", trigger = "action_Settings", size = "small",
+          h3("Time"), 
+          fluidRow(column(6, offset = 1, checkboxInput("checkbox_24h", label = "24h format", value = FALSE)))),
+  bsModal(id = "modal_About", "About", trigger = "action_About", size = "large",
+          #h4("By Peter Hanula, Dimitar Kirilov, Tarush Vig"),
+          h3("Project 2 - CS 424", align = "center"),
+          h3("Learning to Fly", align = "center"),
+          h4("By: Tarush Vig (tvig2), Peter Hanula (phanul2), and Dimitar Kirilov (dkiril4).", align = "center"),
+          h5("DATA INFORMATION", align = "center"),
+          h6("The data for this project comes from The Bureau of Transportation Statistics of The United States Department of Transportation."),
+          h6("Direct link: https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236&DB_Short_Name=On-Time"),
+          h5("APPLICATION INFORMATION", align = "center"),
+          h6("The purpose of this application is to extrapolate upon the data given regarding the flight statistics for the state of Illinois and create dynamic/interactive visualizations to obtain a clearer picture of flight activity, broken down into multiple components (i. e. types of airlines, arrival/departure airport frequencies, hourly/weekly/monthly frequencies, etc.). This application can be run on a multi-display wall."),
+          h5("LIBRARIES USED", align = "center"),
+          h6("- shiny", align = "left"),
+          h6("- shinydashboard", align = "left"),
+          h6("- plyr", align = "left"),
+          h6("- ggplot2", align = "left"),
+          h6("- lubridate", align = "left"),
+          h6("- dplyr", align = "left"),
+          h6("- reshape2", align = "left"),
+          h6("- rsconnect", align = "left"),
+          h6("- tidyr", align = "left"),
+          h6("- magrittr", align = "left"),
+          h6("- DT", align = "left"),
+          h6("- streamgraph", align = "left"),
+          h6("- plotly", align = "left"))
+
+)
+
